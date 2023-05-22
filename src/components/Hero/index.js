c1770ca8f8b5dc730c4871cfa910924888b097bd
Darvi Sunny <darvi.sunny@tadigital.com>
2023-05-22 19:09:40 +0530

    Staging Changes

---
+++ src/components/Hero/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { RichText, Image } from '@sitecore-jss/sitecore-jss-react';
+function Hero({ fields }) {
+  return (
+    <div className="hero">
+      <div className="heroImg">
+        <Image field={fields.heroimg} />
+      </div>
+      {console.log(fields)}
+      <React.Fragment>
+        <RichText
+          field={fields.herotext}
+          tag="section"
+          className="herotext"
+          editable={false}
+          data-sample="other-attributes-pass-through"
+        />
+      </React.Fragment>
+      <div className="heroCTA">
+        <button>FIND YOUR WATCH</button>
+        <button>FIND A BOUTIQUE</button>
+        <button>FIND YOUR STRAP</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
